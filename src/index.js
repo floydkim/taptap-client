66d928381995f0a9deaba76f9b5569a811805aaf
SeongJun Park <tjdwns4850@gmail.com>
2019-03-29 10:13:10 +0900

    불필요한 코드 제거

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const ff = () => {
-  let x = 0;
-};
-
 ReactDOM.render(<App />, document.getElementById('root'));
